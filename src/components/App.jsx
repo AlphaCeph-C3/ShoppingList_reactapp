ef3ea5bbf9ad3e5ed59fc1282e5ee406175b258e
AlphaCeph-C3 <101421534+AlphaCeph-C3@users.noreply.github.com>
2023-06-11 01:49:48 +0530

    Controlled Components inputs completed!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,68 @@
 import { useState } from "react";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
+import SearchItem from "./SearchItem";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
+    const [items, setItems] = useState(
+        JSON.parse(localStorage.getItem("shoppinglist"))
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
+    const [search, setSearch] = useState("");
 
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id && item);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
+    const [newItem, setNewItem] = useState("");
 
-  return (
-    <div className="App">
-      <Header title="Groceries List" />
-      <Content
-        items={items}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
-      <Footer length={items.length} />
-    </div>
-  );
+    const setAndSaveItems = (newItems) => {
+        setItems(newItems);
+        localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+    };
+
+    const addItem = (item) => {
+        const id = items.length ? items[items.length - 1].id + 1 : 1;
+        const myNewItem = { id: id, checked: false, item: item };
+        const listItems = [...items, myNewItem];
+        setAndSaveItems(listItems);
+    };
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        setAndSaveItems(listItems);
+    };
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id && item);
+        setAndSaveItems(listItems);
+    };
+
+    const handleSubmit = (e) => {
+        if (!newItem.trim()) return;
+        addItem(newItem);
+        setNewItem("");
+        e.preventDefault();
+    };
+
+    return (
+        <div className="App">
+            <Header title="Groceries List" />
+            <AddItem
+                newItem={newItem}
+                setNewItem={setNewItem}
+                handleSubmit={handleSubmit}
+            />
+            <SearchItem search={search} setSearch={setSearch} />
+            <Content
+                items={items.filter((item) =>
+                    item.item.toLowerCase().includes(search.toLowerCase())
+                )}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+            />
+            <Footer length={items.length} />
+        </div>
+    );
 }
 
 export default App;
