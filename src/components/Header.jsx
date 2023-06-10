a2d2f89a141d5792c0d500dad48f4e5779f2d2f5
AlphaCeph-C3 <101421534+AlphaCeph-C3@users.noreply.github.com>
2023-06-10 22:21:59 +0530

    Props and Props Drilling learnt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+
+// Default properties for the header component if we have no idea what props would be sent.
+Header.defaultProps = {
+  title: "Default title",
 };
 
 export default Header;
