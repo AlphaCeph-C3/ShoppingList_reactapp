66417ca6eeea0e7800049fb557d778280e97d044
AlphaCeph-C3 <101421534+AlphaCeph-C3@users.noreply.github.com>
2023-06-19 14:20:54 +0530

    router_v5 and the routes and links for the blog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,109 @@
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+
 function App() {
-  return <main className="App">Hello World</main>;
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      datetime,
+      body: postBody,
+    };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    history.push("/");
+  };
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter((post) => post.id !== id);
+    setPosts(postsList);
+    history.push("/");
+  };
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      <Switch>
+        <Route exact path="/">
+          <Home posts={searchResults} />
+        </Route>
+        <Route exact path="/post">
+          <NewPost
+            postTitle={postTitle}
+            postBody={postBody}
+            handleSubmit={handleSubmit}
+            setPostTitle={setPostTitle}
+            setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage posts={posts} handleDelete={handleDelete} />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+        {/* <Route path="/layout">
+        <Layout />
+      </Route> */}
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React-Router package has underwent changes from version 5 to 6 refer to documentation
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/*" component={App} />
+    </Router>
   </React.StrictMode>
 );
